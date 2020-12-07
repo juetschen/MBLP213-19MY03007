dc1e6aba59c5681e5860d9c61075da889cc0d36f
juetschen <60485447+juetschen@users.noreply.github.com>
2020-12-07 22:29:20 +0300

    İsletim_sistemleri_vize

---
+++ enes.ps1
@@ -0,0 +1,14 @@
+﻿$Processes = @{}
+Get-Process  | ForEach-Object {
+    $Processes[$_.Id] = $_
+}
+
+Get-NetTCPConnection | 
+    Where-Object { $_.State } |
+    Select-Object RemoteAddress,
+        RemotePort,
+        @{Name="PID";         Expression={ $_.OwningProcess }},
+        @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }},
+        @{Name="State"; Expression={ $_.State  }}|
+    Sort-Object -Property ProcessName, UserName |
+    Format-Table -AutoSize | Out-File C:\Temp\1231.txt -Encoding utf8
